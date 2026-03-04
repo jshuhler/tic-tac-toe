023095fdfac7879aedc716c5581ee6f84a04baca
jshuhler <15068877+jshuhler@users.noreply.github.com>
2026-03-04 16:40:07 -0500

    starting to check for winners

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const gameBoard = (() => {
     for (let row = 0; row < rows; row++) {
         board.push([]);
         for (let col = 0; col < columns; col++)
-            board[row].push(`row:${row},col:${col}`); //change to push(col) to see col numbers in console
+            board[row].push("_"); //change to push(col) to see col numbers in console
     };
 
     // printing the game board to the console
@@ -33,13 +33,34 @@ function gameController(row,col,board) {
     const playerTwo = createPlayer("Player Two", "O");
 
     // playing a turn
-    const playTurn = (player, row, col, board) => {
+    const playTurn = (player, row, col) => {
         // checking for the correct board location and layout
         console.log(gameBoard.board[row][col]);
         console.log(gameBoard.board);
 
         //replacing the existing board marker (blank) with the player marker (X or O)
-        gameBoard.board[row].splice(col,1,player.marker);
+        if (gameBoard.board[row][col] === "_") {
+            gameBoard.board[row].splice(col,1,player.marker);
+            checkWinner();
+        } else {
+            console.log("That space is already taken, try again");
+        }
+    }
+
+    // check if someone has won, will need to 
+    // be called after each turn I think
+    const checkWinner = (player) => {
+        // checking row winner
+        for (let i = 0; i < 3; i++) {
+            if (gameBoard.board[i][0] === player.marker && gameBoard.board[i][1] === player.marker && gameBoard.board[i][2]) {
+                return true;
+            }
+        }
+
+        // checking col winner
+
+
+        // checking diagonal winner
     }
 
     return { playerOne, playerTwo, playTurn };
